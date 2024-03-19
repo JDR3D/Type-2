e9a389be714568cdf2bfd014826ecfb78c445333
JDR3D <jrouzeval@gmail.com>
2024-03-20 00:38:54 +0100

    commit

---
+++ app-files/data.js
@@ -0,0 +1,365 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-t2---sjour-1",
+      "name": "T2 - Séjour 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.18949953862807334,
+        "pitch": 0.005379097879897543,
+        "fov": 1.1848132852457431
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.39844556717816815,
+          "pitch": -0.02832302304875789,
+          "rotation": 0,
+          "target": "0-t2---sjour-1"
+        },
+        {
+          "yaw": -2.103605228087213,
+          "pitch": -0.05232785960772901,
+          "rotation": 0,
+          "target": "4-t2---couloir"
+        },
+        {
+          "yaw": -2.876257111805044,
+          "pitch": -0.04229184103223815,
+          "rotation": 0,
+          "target": "5-t2---salle-de-bain"
+        },
+        {
+          "yaw": 1.4602355780826057,
+          "pitch": -0.03715310802331295,
+          "rotation": 0,
+          "target": "3-t2---chambre"
+        },
+        {
+          "yaw": 0.5463632951983044,
+          "pitch": -0.02291725633256192,
+          "rotation": 0,
+          "target": "2-t2---terrasse"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-t2---sjour-2",
+      "name": "T2 - Séjour 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 2.2688383222249637,
+        "pitch": -0.004759214806890455,
+        "fov": 1.219533680470021
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3272582741353354,
+          "pitch": 0.0060422729777087625,
+          "rotation": 0,
+          "target": "2-t2---terrasse"
+        },
+        {
+          "yaw": 2.8798228302968685,
+          "pitch": 0.008007275799871039,
+          "rotation": 0,
+          "target": "0-t2---sjour-1"
+        },
+        {
+          "yaw": 2.62194119378697,
+          "pitch": 0.006935169255198659,
+          "rotation": 0,
+          "target": "3-t2---chambre"
+        },
+        {
+          "yaw": 3.0719478078297353,
+          "pitch": 0.012500772221521572,
+          "rotation": 0,
+          "target": "5-t2---salle-de-bain"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-t2---terrasse",
+      "name": "T2 - Terrasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.23311209656759146,
+        "pitch": 0.004522542722817491,
+        "fov": 1.1551302309345253
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.010659031808551,
+          "pitch": 0.0028797582351174356,
+          "rotation": 0,
+          "target": "1-t2---sjour-2"
+        },
+        {
+          "yaw": 2.117120864916819,
+          "pitch": -0.0010919312146313587,
+          "rotation": 0,
+          "target": "0-t2---sjour-1"
+        },
+        {
+          "yaw": 1.2776344649031657,
+          "pitch": -0.01648191045950398,
+          "rotation": 0,
+          "target": "3-t2---chambre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-t2---chambre",
+      "name": "T2 - Chambre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": -0.8841783046873992,
+        "pitch": -0.0018304528405366227,
+        "fov": 1.0872795711561583
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49106251806003165,
+          "pitch": 0.025145692788610674,
+          "rotation": 0,
+          "target": "0-t2---sjour-1"
+        },
+        {
+          "yaw": -0.7007145839130544,
+          "pitch": 0.027269613929707504,
+          "rotation": 0,
+          "target": "4-t2---couloir"
+        },
+        {
+          "yaw": 0.770476867020097,
+          "pitch": 0.010209072710823719,
+          "rotation": 0,
+          "target": "2-t2---terrasse"
+        },
+        {
+          "yaw": 0.452499294851183,
+          "pitch": 0.014988491769678092,
+          "rotation": 0,
+          "target": "1-t2---sjour-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-t2---couloir",
+      "name": "T2 - Couloir",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.1780352847229416,
+        "pitch": -0.008167560013859543,
+        "fov": 1.032628077535456
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10411452233986651,
+          "pitch": -0.009248405128095882,
+          "rotation": 0,
+          "target": "0-t2---sjour-1"
+        },
+        {
+          "yaw": 0.11767239917325867,
+          "pitch": -0.004461222762934369,
+          "rotation": 0,
+          "target": "3-t2---chambre"
+        },
+        {
+          "yaw": 0.3505384590388765,
+          "pitch": -0.0014298342594898372,
+          "rotation": 0,
+          "target": "5-t2---salle-de-bain"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-t2---salle-de-bain",
+      "name": "T2 - Salle de bain",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 1.1095129400249668,
+        "pitch": -0.006177387156395042,
+        "fov": 1.2323003685742213
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3169852545973733,
+          "pitch": -0.020399433263760258,
+          "rotation": 0,
+          "target": "4-t2---couloir"
+        },
+        {
+          "yaw": 2.9058318966120043,
+          "pitch": 0.35592880336334254,
+          "rotation": 0,
+          "target": "0-t2---sjour-1"
+        },
+        {
+          "yaw": 2.526974353859731,
+          "pitch": 0.03410993752086,
+          "rotation": 0,
+          "target": "1-t2---sjour-2"
+        },
+        {
+          "yaw": 3.069775982827653,
+          "pitch": 0.01644811867832985,
+          "rotation": 0,
+          "target": "2-t2---terrasse"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
